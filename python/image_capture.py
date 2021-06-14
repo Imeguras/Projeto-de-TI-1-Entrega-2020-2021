b38ede00bf1556a52967c9fbbba6b311c7eff491
AndreGSSampaio <83128736+AndreGSSampaio@users.noreply.github.com>
2021-06-14 14:03:21 +0100

    Everything is finalized

--- python/image_capture.py
+++ python/image_capture.py
@@ -2,38 +2,38 @@ import cv2 as cv
 import time
 import requests
 
-def send_file():
-    files = {'imagem': open('webcam.jpg', 'rb')}
-    r = requests.post('http://127.0.0.1/ti/upload.php', files=files)
-    print(r.status_code, r.text)
+phoneip = "http://192.168.1.163:8080"
+
+def send_status():
+    val = {'tipo':'atuadores', 'nome':'camara', 'valor':'0'}
+    r = requests.post('http://127.0.0.1/ProjetoTI/api/api.php', val)
 
 def image_capture():
-
     try:
-        cv.VideoCapture(0)
+        camera = cv.VideoCapture(f"{phoneip}/video")
     except:
-        camera = cv.VideoCapture("http://10.20.229.19:4747/video")
+        camera = cv.VideoCapture(0)
     ret, image = camera.read()
-    cv.imwrite('webcam.jpg', image)
+    cv.imwrite('Ficheiros/Camara.jpg', image)
     camera.release()
     cv.destroyAllWindows()
     
 
 try:
     while True:
-        r=requests.get('http://127.0.0.1/ti/api/api.php?nome=botao')
+        r=requests.get('http://127.0.0.1/ProjetoTI/api/api.php?tipo=atuadores&nome=camara')
         status = r.status_code
         if status == 200:
-            value = float(r._content)
-            if value > 20.0:
+            value = int(r._content)
+            if value == 1:
                 image_capture()
-                send_file()
+                send_status()
             else:
                 pass
         else:
             print("Erro ao ler do servidor")
-        time.sleep(5)
-except:
+        time.sleep(1)
+except KeyboardInterrupt:
     print("Leaving...")
 
 #img = cv.imread('opencv_image.png', 0)
